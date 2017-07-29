a94af0b464df98ec10dc66c8ee9a299899651c32
Brandon Tat <brandontat7@gmail.com>
2017-07-28 19:30:55 -0700

    add cat model

---
+++ app/models/cat.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :integer          not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Cat < ApplicationRecord
+  CAT_COLORS = %w(black white blue red orange)
+
+  validates(
+    :birth_date,
+    :color,
+    :name,
+    :sex,
+    presence: true
+  )
+
+  validates :color, inclusion: CAT_COLORS
+  validates :sex, inclusion: %w(M F)
+end
